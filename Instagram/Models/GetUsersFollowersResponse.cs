3e755da4c1a38e4efe52aea0f0deaf7e35bbb3fa
Dmitry Evmenov <DmitryEvmenov@coherentsolutions.com>
2018-04-01 16:00:48 +0300

    Added basic insta responses

---
+++ Instagram/Models/GetUsersFollowersResponse.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Instagram.Models
+{
+    public partial class GetUsersFollowersResponse
+    {
+        [JsonProperty("data")]
+        public GetUsersFollowersResponseData[] Data { get; set; }
+    }
+
+    public partial class GetUsersFollowersResponseData
+    {
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("profile_picture")]
+        public string ProfilePicture { get; set; }
+
+        [JsonProperty("full_name")]
+        public string FullName { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+    }
+}
---
+++ Instagram/Models/SearchUsersResponse.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace Instagram.Models
+{
+
+
+    public partial class SearchUsersResponse
+    {
+        [JsonProperty("data")]
+        public SearchUsersResposeData[] Data { get; set; }
+    }
+
+    public partial class SearchUsersResposeData
+    {
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("profile_picture")]
+        public string ProfilePicture { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+    }
+}
